4cb86320f84096a10253f56180536b32026e6b7f
GenericOctopus <genericoctopus@gmail.com>
2022-11-15 21:55:39 -0500

    mock-up with array list inside ionic cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  todoList = [{
+    itemName: 'Build Ionic App Front',
+    itemDueDate: '11-17-2022',
+    itemPriority: 'high',
+    itemCategory: 'Code'
+  },
+  {
+    itemName: 'Ionic App Backend',
+    itemDueDate: '11-18-2022',
+    itemPriority: 'medium',
+    itemCategory: 'Code'
+  },
+  {
+    itemName: 'Publish Ionic App',
+    itemDueDate: '11-19-2022',
+    itemPriority: 'low',
+    itemCategory: 'Code'
+  },
+]
+
+  today: number = Date.now()
   constructor() {}
 
 }
